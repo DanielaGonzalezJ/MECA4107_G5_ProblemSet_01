aa97e3f009eed7b8e4497bc4184f859310d78fd2
Daniela Gonzalez J <158006614+DanielaGonzalezJ@users.noreply.github.com>
2024-02-17 15:00:53 -0500

    Update 2_Scraping.R

--- scripts/2_Scraping.R
+++ scripts/2_Scraping.R
@@ -42,34 +42,18 @@ my_url_ext <- ".html"
 my_urls <- paste0(my_url_base, my_url_pages, my_url_ext)  # Concatenate all elements
 
 my_html = list()
-<<<<<<< Updated upstream
-div = list()
-=======
 table_1 = list()
->>>>>>> Stashed changes
 
 for (i in 1:length(my_urls)) {
   url <- my_urls[i]
   #browseURL(url)  # Print the full URL
   my_html[[i]] = read_html(url)
-<<<<<<< Updated upstream
-  class(my_html[[i]]) ## ver la clase del objeto
-  #view(my_html[[i]])
-  div[[i]]<- my_html[[i]] %>% html_elements("div") %>%
-    html_elements("a") %>%
-    html_text2()
-    }
-
-#my_html[[1]] %>% html_elements("h4")
-
-
-#----------------------------------------------------------------------------
-=======
   table_1[[i]] <- my_html[[i]] %>%  html_table() 
   #table_1[[i]] <- as.data.frame()
     }
-#----------------------------------------------------------------------------
+
 my_html[[1]]
 table_1[[1]]
 
->>>>>>> Stashed changes
+
+#----------------------------------------------------------------------------
