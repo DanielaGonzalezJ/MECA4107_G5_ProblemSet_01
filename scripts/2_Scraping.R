8de2e0fbde480d6196d852666ab911ee00228151
Dago000 <de.gonzalezg1@uniandes.edu.co>
2024-03-03 17:29:51 -0500

    Update 2_Scraping.R

--- scripts/2_Scraping.R
+++ scripts/2_Scraping.R
@@ -26,3 +26,21 @@ setwd("C:/Users/USER/OneDrive - Universidad de los andes/Semestre VIII/Big Data/
 p_load(haven,skimr, tidyverse, forecast, ggplot2, tseries, readxl, writexl, dplyr, 
        fable, fpp2, lubridate, mFilter, urca, stargazer, quantmod, margins,ggeffects,rvest,grid,gridExtra,psych,xtable)
 
+####Cargar Datos (Scraping)####
+
+my_url = "https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_"
+class(my_url)
+
+table_u=list()
+table=list()  
+
+#Recorrido de todos los Chunks
+for (i in 1:10){
+  url=paste0(my_url,i,".html")
+  print(url)
+  my_html = read_html(url)
+  table<- my_html %>% 
+    html_table()
+  table<- as.data.frame(table)
+  table_u <- rbind(table_u,table)
+}
