fe4e639e44126b03118660de3073834cfc69ae34
Dago000 <de.gonzalezg1@uniandes.edu.co>
2024-03-03 17:49:58 -0500

    Update 2_Scraping.R

--- scripts/2_Scraping.R
+++ scripts/2_Scraping.R
@@ -21,7 +21,7 @@ rm(list=ls())
 if(!require(pacman)) install.packages("pacman") ; require(pacman)
 
 require(pacman)
-setwd("C:/Users/USER/OneDrive - Universidad de los andes/Semestre VIII/Big Data/Taller I")
+setwd("C:/Users/USER/OneDrive - Universidad de los andes/Semestre VIII/Big Data/Repositorios/MECA4107_G5_ProblemSet_01/scripts")
 
 p_load(haven,skimr, tidyverse, forecast, ggplot2, tseries, readxl, writexl, dplyr, 
        fable, fpp2, lubridate, mFilter, urca, stargazer, quantmod, margins,ggeffects,rvest,grid,gridExtra,psych,xtable)
@@ -84,3 +84,51 @@ c <- ggplot(table, aes(x = estrato1 )) +
 d <- grid.arrange(a, b, c, ncol = 3)
 
 ggsave("Distribucion_1.png", plot = d, units = "in")
+
+##### Imputación de Ingreso por Estrato para reportes de 0####
+
+table = table %>% 
+  group_by(estrato1) %>% 
+  mutate(mean_ingtot = mean(ingtot, na.rm=T)) %>% 
+  ungroup()
+
+table = table %>% 
+  mutate(ingtot_1 = ifelse(ingtot==0, 
+                           yes =mean_ingtot, 
+                           no=ingtot )) 
+table<-  table %>%
+  mutate(log_ingtot_1= log(ingtot_1))
+
+#Revsisión del resultados de distribución
+e <- ggplot(table, aes(x = log_ingtot_1 )) +
+  geom_histogram(bins = 50, fill = "darkblue") +
+  labs(x = "Ingreso (Imputado por Estrato) en Log", y = "N. Obs") +
+  theme_classic() 
+
+ggsave("Distribucion_2.png", plot = e, units = "in")
+
+#write_xlsx(table, "Base_Filtrada.xlsx")
+
+##### Estadísticas Descriptivas#######
+#Renombrar Variables
+table <- table %>% 
+  rename(Escolaridad = p6210,
+         'Tiempo en este trabajo' = p6426,
+         Sexo = sex,
+         Edad=age,
+         'Parentesco con el jefe del hogar'=p6050,
+         Regimen=p6100,
+         'Horas trabajadas semanal'=hoursWorkUsual,
+         'Ingreso Observado'=ingtotob,
+         Estrato=estrato1,
+         'Ingreso Imputado'=ingtot_1,
+         Oficio=oficio
+  )
+
+#seleccionar Variables de interés y exportar reportes en txt y latex
+estadisticas <- select(table,Edad,Sexo,'Ingreso Observado','Ingreso Imputado',Estrato,'Tiempo en este trabajo', 'Horas trabajadas semanal','Parentesco con el jefe del hogar','Oficio')
+estadisticas <- data.frame(estadisticas)
+stargazer(estadisticas,type = "text", title="Estadísticas Descriptivas",out="Estadisticas_Descriptivas_text",style="aer")
+stargazer(estadisticas, type = "latex", title="Estadísticas Descriptivas",out="Estadisticas_Descriptivas_Latex",style="aer")
+
+
