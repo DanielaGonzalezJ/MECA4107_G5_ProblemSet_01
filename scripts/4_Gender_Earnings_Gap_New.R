9c09a60861928dba5e43359c3e02d9affea433d5
Daniela Gonzalez J <158006614+DanielaGonzalezJ@users.noreply.github.com>
2024-03-03 23:35:20 -0500

    Punto_4_Nuevo

--- scripts/4_Gender_Earnings_Gap_New.R
+++ scripts/4_Gender_Earnings_Gap_New.R
@@ -24,52 +24,29 @@ table_4$sex_1 <- -1*(table_4$sex-1)
 reg1<-lm(log_ingtot_1 ~ sex_1, data =table_4)
 
 
-
-
 ## b. Equal Pay for Equal Work:
 
-<<<<<<< Updated upstream
 
 reg2 <- lm(log_ingtot_1 ~ sex_1 + age +  I(sex_1*age) + factor(oficio) + factor(college), data =table_4) 
 
 
 #a & b
 stargazer(reg1, reg2, omit="oficio",type="latex",digits=2)
-=======
-##Reg2
-
-
-#reg2 <- lm(log_ingtot_1 ~ sex_1 + factor(age) + factor(oficio) + factor(college), data =table_4)  
-#stargazer(reg2,type="text",digits=7)
-
-reg3 <- lm(log_ingtot_1 ~ sex_1 + age , data =table_4)  
-stargazer(reg3,type="text",digits=7)
-
->>>>>>> Stashed changes
 
 
 ## FWL
 
 #Regress X1 ~ sex_1 on X2 ~ age and take the residuals
 
-<<<<<<< Updated upstream
 table_4<-table_4 %>% mutate(sexResidF=lm(sex_1 ~ age  +  I(sex_1*age) + factor(oficio) + factor(college) ,table_4)$residuals) #Residuals of weight~foreign 
 
 #Regress Y on X2 and take the residuals
 
 table_4<-table_4 %>% mutate(litResidF=lm(log_ingtot_1 ~ age  +  I(sex_1*age)+ factor(oficio) + factor(college), table_4)$residuals) #Residuals of mpg~foreign 
-=======
-table_4<-table_4 %>% mutate(sexResidF=lm(sex_1 ~ age ,table_4)$residuals) #Residuals of weight~foreign 
-
-#Regress Y on X2 and take the residuals
-
-table_4<-table_4 %>% mutate(litResidF=lm(log_ingtot_1 ~ age, table_4)$residuals) #Residuals of mpg~foreign 
->>>>>>> Stashed changes
 
 #Regress the residuals from step 2 on the residuals from step 1
 reg4<-lm(litResidF~sexResidF , table_4)
 
-<<<<<<< Updated upstream
 stargazer(reg2,reg4, omit="oficio", type="latex",digits=2) # with stargazer we can visualize the coefficients next to each other
 
 
@@ -78,16 +55,6 @@ stargazer(reg2,reg4, omit="oficio", type="latex",digits=2) # with stargazer we c
 
 beta_1 <- function(data, index){
   coef(lm(log_ingtot_1 ~ sex_1 + age  +  I(sex_1*age) + factor(oficio) + factor(college), data = table_4 , subset = index))[2]
-=======
-stargazer(reg3,reg4,type="text",digits=7) # with stargazer we can visualize the coefficients next to each other
-
-
-## FWL with Bootstrap
-
-
-beta_1 <- function(data, index){
-  coef(lm(log_ingtot_1 ~ sex_1 + age, data = table_4 , subset = index))[2]
->>>>>>> Stashed changes
 }
 
 beta_1(table_4, 1:nrow(table_4))
@@ -95,30 +62,20 @@ beta_1(table_4, 1:nrow(table_4))
 
 set.seed(86508)
 #Call boot
-<<<<<<< Updated upstream
 boot_result_OLS <- boot(table_4, beta_1, R=1000)
 
 
 
 ##FWL with bootstrap
-=======
-boot(table_4, beta_1, R=1000)
-
-
-
-##Test
->>>>>>> Stashed changes
 beta_1_1 <- function(data,index){
   coef(lm(litResidF~sexResidF , data = table_4 , subset = index))[2]
 }
 
 beta_1_1(table_4, 1:nrow(table_4))
-<<<<<<< Updated upstream
 
 set.seed(86508)
 #Call boot
 boot_result_FWL <- boot(table_4, beta_1_1, R=1000)
-
 
 ## Plot and Peak Age Estimation
 
@@ -146,13 +103,3 @@ lines(age_range, male_wages, type = "l", col = "red", lwd = 2)
 legend("topright", legend = c("Female", "Male"), col = c("blue", "red"), 
        lty = 1, lwd = 2)
 
-=======
-##
-
-set.seed(86508)
-#Call boot
-boot(table_4, beta_1_1, R=1000)
-
-
-## Plot 
->>>>>>> Stashed changes
